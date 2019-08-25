70638a3c4bcf636e52078e3e57308e50629f756b
Julian Santana <jjsantan@my.loyno.edu>
2019-08-25 02:35:06 -0400

    playing around

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-ENV['REDIS_URL'] = ENV['REDISCLOUD_URL'] || 'ppp'
-if ENV['REDISCLOUD_URL']
-  ENV['REDIS_URL'] = ENV['REDISCLOUD_URL']
-  uri = URI.parse(ENV['REDISCLOUD_URL'])
-  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
- end
+# ENV['REDIS_URL'] = ENV['REDISCLOUD_URL'] if ENV['REDISCLOUD_URL']
+# if ENV['REDISCLOUD_URL']
+# ENV['REDIS_URL'] = ENV['REDISCLOUD_URL']
+#  uri = URI.parse(ENV['REDISCLOUD_URL'])
+#  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+# end
