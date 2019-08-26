0b78a47f352255842250ba7602fb573e259eb563
Julian Santana <jjsantan@my.loyno.edu>
2019-08-26 00:16:48 -0400

    resque production config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,5 @@ if ENV['REDISCLOUD_URL']
   ENV['REDIS_URL'] = ENV['REDISCLOUD_URL']
   uri = URI.parse(ENV['REDISCLOUD_URL'])
   $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  Resque.redis = $redis
 end
