309236177db62db2552f8384860f0e9092ecfa77
Julian Santana <jjsantan@my.loyno.edu>
2019-08-26 02:34:42 -0400

    more specific test on job

--- spec/jobs/broadcast_message_job_spec.rb
+++ spec/jobs/broadcast_message_job_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe BroadcastMessageJob, type: :job do
   it 'queues the job' do
     task = FactoryBot.create :task
-    expect { BroadcastMessageJob.perform_later(task.department, task) }.to have_enqueued_job(BroadcastMessageJob)
+    expect { BroadcastMessageJob.perform_later(task.department, task) }.to have_enqueued_job(BroadcastMessageJob).with(task.department, task).at(:no_wait)
   end
 end
